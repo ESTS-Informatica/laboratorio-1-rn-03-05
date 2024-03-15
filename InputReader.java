bdb681ce65beba5aa320d88d3d40e7a178b44bdb
Dinis-Xavier <202300148@estudantes.ips.pt>
2024-03-15 10:05:00 +0000

    Nível 2
    Nível 2

--- InputReader.java
+++ InputReader.java
@@ -27,4 +27,6 @@ public class InputReader {
 
         return answer;
     }
+    
+    
 }
--- WorldGuessingGame.java
+++ WorldGuessingGame.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 
 /**
  * Escreva uma descrição da classe WorldGuessingGame aqui.
@@ -9,6 +10,7 @@ public class WorldGuessingGame
 {
     private String hiddenWord;
     private String guessedWord;
+    private InputReader reader;
     private int numberOfTries;
     
     //Construtor
@@ -16,6 +18,7 @@ public class WorldGuessingGame
      hiddenWord = "abc";
      numberOfTries = 0;
      guessedWord = "___";
+     reader= new InputReader();
     }
     
     //Métodos Seletores 
@@ -39,5 +42,43 @@ public class WorldGuessingGame
       System.out.println("A palavra que estás a adivinhar é " + guessedWord);
     }
     
+    //Método showWelcome
+    private void showWelcome(){
+       System.out.println("Bem vindo ao jogo de adivinhar a palavra");
+       System.out.println("A palavra que tens de adivinhar tem "+hiddenWord.length() + "letras");
+    }
+    
+    private void guess(char letter) {
+        boolean found = false;
+        for (int i = 0; i < hiddenWord.length(); i++) {
+            if (hiddenWord.charAt(i) == letter && guessedWord.charAt(i) == '_') {
+                guessedWord = guessedWord.substring(0, i) + letter + guessedWord.substring(i + 1);
+                found = true;
+            }
+        }
+        if (found) {
+            System.out.println("Letra correta!");
+        } else {
+            System.out.println("Letra incorreta!");
+        }
+    }
+    
+    private void showResult(){
+     System.out.println("já tentás-te adivinhar a palavra "+numberOfTries+ " vezes");
+    }
+    
+    public void play (){
+       showWelcome();
+       Scanner scanner = new Scanner(System.in);
+        while (!hiddenWord.equals(guessedWord)) {
+            showGuessedWord();
+            System.out.print("Digite uma letra: ");
+            char letter = scanner.next().charAt(0);
+            guess(letter);
+            numberOfTries++;
+        }
+        showResult();
+        scanner.close();
+    }
     
 }
